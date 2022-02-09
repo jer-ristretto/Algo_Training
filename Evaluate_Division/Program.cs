f75df78ba8f96dbdd018996eb24a14a7881e9ef5
Jeremy Xiao <s3829221@student.rmit.edu.au>
2022-02-09 17:15:54 +1100

    Incomplete Evaluate Division problem
    - Implemented union find
    - Suggest considering DFS

---
+++ Evaluate_Division/Program.cs
@@ -0,0 +1,28 @@
+﻿using Evaluate_Division;
+
+var solution = new Solution();
+
+IList<IList<string>> equations = new List<IList<string>>();
+equations.Add(new List<string> {"a", "b"});
+equations.Add(new List<string> {"b", "c"});
+equations.Add(new List<string> {"c", "d"});
+equations.Add(new List<string> {"d", "e"});
+
+double[] values = {3.0, 4.0, 5.0, 6.0};
+
+IList<IList<string>> queries = new List<IList<string>>();
+queries.Add(new List<string> {"a", "e"});
+queries.Add(new List<string> {"e", "b"});
+queries.Add(new List<string> {"b", "d"});
+queries.Add(new List<string> {"b", "b"});
+queries.Add(new List<string> {"b", "h"});
+queries.Add(new List<string> {"h", "h"});
+
+var results = solution.CalcEquation(equations, values, queries);
+
+foreach (var result in results)
+{
+	Console.Write(result + " ");
+}
+
+Console.WriteLine();
---
+++ Evaluate_Division/Solution.cs
@@ -0,0 +1,89 @@
+﻿namespace Evaluate_Division;
+
+public class Solution
+{
+	// Key: variable	Value: (parent, factor of parent)
+	public Dictionary<string, (string, double)> Graph { get; set; }
+	
+	public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
+	{
+		Graph = new Dictionary<string, (string, double)>();
+		
+		var graphSize = values.Length;
+		
+		// Construct the graph
+		for (int i = 0; i < graphSize; i++)
+		{
+			Union(equations[i][0], equations[i][1], values[i]);
+		}
+		
+		var resultSize = queries.Count;
+		var results = new double[resultSize];
+		
+		// Compute the result
+		for (int i = 0; i < resultSize; i++)
+		{
+			var varX = queries[i][0];
+			var varY = queries[i][1];
+			if (!Graph.ContainsKey(varX) || !Graph.ContainsKey(varY))
+			{
+				results[i] = -1;
+				continue;
+			}
+
+			var rootX = Find(varX);
+			var rootY = Find(varY);
+
+			if (!rootX.Equals(rootY))
+			{
+				results[i] = -1;
+				continue;
+			}
+
+			var (_, factorX) = Graph[varX];
+			var (_, factorY) = Graph[varY];
+
+			results[i] = (double) factorX / factorY;
+		}
+
+		return results;
+	}
+
+	public void Union(string x, string y, double factor)
+	{
+		Graph.TryAdd(x, (x, 1));
+		Graph.TryAdd(y, (y, 1));
+		
+		// Find and update the root and factor on the path
+		var rootX = Find(x);
+		var rootY = Find(y);
+
+		if (rootX.Equals(rootY))
+			return;
+
+		var (_, xToRootXFactor) = Graph[x];
+		var (_, yToRootYFactor) = Graph[y];
+
+		var newFactor = yToRootYFactor / xToRootXFactor * factor;
+
+		Graph[x] = (rootY, newFactor);
+	}
+	
+	public string Find(string x)
+	{
+		if (!Graph.ContainsKey(x))
+			return null;
+		
+		var (parent, factor) = Graph[x];
+
+		if (parent.Equals(x))
+			return x;
+
+		var newParent = Find(parent);
+		var newFactor = factor * Graph[parent].Item2;
+
+		Graph[x] = (newParent, newFactor);
+
+		return newParent;
+	}
+}
